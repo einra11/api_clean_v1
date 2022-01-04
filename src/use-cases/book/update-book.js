fe17deca9b0312f7509cc23455620e9f219d9288
einra11 <kennethclone55@gmail.com>
2022-01-04 16:39:44 +0800

    adjustments

--- src/use-cases/book/update-book.js
+++ src/use-cases/book/update-book.js
@@ -12,18 +12,14 @@ const updateBook = ({ bookDB, updateBook_ENTITY }) => {
             let entity = await updateBook_ENTITY({ prevData, data });
             const res = await bookDB.updateBook({entity});
 
-            let result = {};
-
 
             if (res) {
-                result.title = res.title;
-                result.author = res.author;
-                result.rating = res.ratings;
-                result.serial = res.serial;
-    
                 return {
-                    message: "Book updated succesfully",
-                    book: { result }
+                message: "Book updated succesfully",
+                   title : res.title,
+                   author : res.author,
+                   rating : res.ratings,
+                   serial : res.serial,
                 }
             }
             else {
