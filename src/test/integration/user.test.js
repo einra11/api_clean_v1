73185943c11ada3d75b8b2f622196e18fb76bbe9
einra11 <kennethclone55@gmail.com>
2022-01-04 13:57:40 +0800

    quick stash

--- src/test/integration/user.test.js
+++ src/test/integration/user.test.js
@@ -1,7 +1,7 @@
 import {app} from '../../app'
 import request from 'supertest'
 
-describe("POST/LOGIN /api/users endpoint", () =>{
+describe("GET/LOGIN /api/users endpoint", () =>{
 
     describe("Given data : email, password", () => {
         it("should return email and token and a status 200", async () =>{
@@ -16,6 +16,68 @@ describe("POST/LOGIN /api/users endpoint", () =>{
         })
     })
 
+    describe("Given data : email, ", () => {
+        it("should return please enter email", async () =>{
+            const response = await request (app)
+            .get("/api/users")
+            .send({
+                password: "123"
+            })
+            console.log(response.body.error|| "No errors found -> Falied")
+            expect(response.body.error).toBe("Please enter email")
+        })
+    })
+
+    describe("Given data : email, password", () => {
+        it("should return please enter password", async () =>{
+            const response = await request (app)
+            .get("/api/users")
+            .send({
+                email: "admin@gmail.com",
+            })
+            console.log(response.body.error|| "No errors found -> Falied")
+            expect(response.body.error).toBe("Please enter password")
+        })
+    })
+
     
+})
+
+describe("POST/REGISTER /api/users endpoint", () =>{
+let email = Buffer.from(Math.random().toString()).toString("base64").substr(10, 5);
+    describe("Given data: email, password, role, status", () =>{
+        it("should return code 200 with no errors", async ()=>{
+            const response = await request(app)
+            .post("/api/users")
+            .send({
+                "email": `${email}@gmail.com`,
+                "password": "123",
+                "role": "god",
+                "status": "active"
+            })
+            console.log(response.body.error|| "No errors found -> Pass")
+            expect(response.statusCode).toBe(200)
+        })
+
+    })
 
 })
+
+
+describe("POST/REGISTER /api/users endpoint", () =>{
+        describe("Given data: , password, role, status", () =>{
+            it("should return please enter email", async ()=>{
+                const response = await request(app)
+                .post("/api/users")
+                .send({
+                    "password": "123",
+                    "role": "god",
+                    "status": "active"
+                })
+                console.log(response.body.error|| "No errors found -> Pass")
+                expect(response.statusCode).toBe(200)
+            })
+    
+        })
+    
+    })
